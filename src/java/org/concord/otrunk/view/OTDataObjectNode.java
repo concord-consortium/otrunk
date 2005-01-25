db76d6568378011afa15b1700f17f22f20bfd5e8
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2005-01-25 16:18:41 +0000

    updated classes to handle frames, delayed blob loading
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@2507 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/OTDataObjectNode.java
+++ src/java/org/concord/otrunk/view/OTDataObjectNode.java
@@ -8,6 +8,7 @@ package org.concord.otrunk.view;
 
 import org.concord.framework.otrunk.OTID;
 import org.concord.framework.otrunk.OTResourceList;
+import org.concord.framework.otrunk.OTrunk;
 import org.concord.framework.util.SimpleTreeNode;
 import org.concord.otrunk.OTrunkImpl;
 import org.concord.otrunk.datamodel.OTDataObject;
@@ -33,17 +34,39 @@ public class OTDataObjectNode
 	
 	public String toString()
 	{
-		// TODO: we need a nicer string representation here
-		// it should be the name of the resource in the parent object
-		// how we handle resouce lists will have to be different
-		return name + ": " + object.getGlobalId().toString();
+		String nodeName = "";
+		
+		String otObjectClass = (String)object.getResource(OTrunkImpl.RES_CLASS_NAME);
+		String otObjectName = (String)object.getResource("name");
+		
+		if(name != null) {
+			if(nodeName.length() > 0) nodeName += " ";
+			nodeName += name;			
+		}		
+		
+		if(otObjectClass != null) {
+			int lastPeriod = otObjectClass.lastIndexOf('.');
+			otObjectClass = otObjectClass.substring(lastPeriod+1);
+			nodeName += "<" + otObjectClass + ">";
+		}
+		
+		if(otObjectName != null) {
+			if(nodeName.length() > 0) nodeName += " ";
+			nodeName += otObjectName;
+		}
+		
+		if(nodeName.length() == 0) {
+			nodeName += object.getGlobalId();
+		}
+		
+		return nodeName;
 	}		
 	
 	public int getChildCount()
 	{
 		if(object != null) {
 			String [] keys = object.getResourceKeys();
-			return keys.length;
+			return keys.length + 1;
 		}
 
 		return 0;
@@ -53,8 +76,13 @@ public class OTDataObjectNode
 	{
 		OTDataObject pfParent = getPfDataObject();
 		
+		if(index == 0) {
+			// make a leaf node that displays the toString of
+			return new OTJavaObjectNode("id", "" + pfParent.getGlobalId());
+		}
+		
 		String [] keys = pfParent.getResourceKeys();
-		String key = keys[index];
+		String key = keys[index - 1];
 		Object child = pfParent.getResource(key);
 			
 		try {		
@@ -84,16 +112,21 @@ public class OTDataObjectNode
 			return new OTJavaObjectNode(key, child);
 		}		
 	}
-	
+
 	public int getIndexOfChild(SimpleTreeNode child)
 	{
 		OTDataObject dataObject = getPfDataObject();
+		
+		if(child.getObject().equals(dataObject.getGlobalId().toString())) {
+			return 0;
+		}
+		
 		String [] keys = dataObject.getResourceKeys();
 		
 		for(int i=0; i<keys.length; i++) {
 			Object testChild = dataObject.getResource(keys[i]);
 			if(child.getObject().equals(testChild)) {
-				return i;
+				return i+1;
 			}			 
 		}
 		
