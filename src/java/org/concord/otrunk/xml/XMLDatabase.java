2866817e3ba5c3e387a7ed27211a8097bd0768f5
maven <maven@6e01202a-0783-4428-890a-84243c50cc2b>
2005-04-11 15:01:08 +0000

    Adding licenses
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@3832 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/xml/XMLDatabase.java
+++ src/java/org/concord/otrunk/xml/XMLDatabase.java
@@ -1,8 +1,32 @@
+
+/*
+ *  Copyright (C) 2004  The Concord Consortium, Inc.,
+ *  10 Concord Crossing, Concord, MA 01741
+ *
+ *  Web Site: http://www.concord.org
+ *  Email: info@concord.org
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 /*
  * Last modification information:
- * $Revision: 1.11 $
- * $Date: 2005-04-05 16:39:54 $
- * $Author: scytacki $
+ * $Revision: 1.12 $
+ * $Date: 2005-04-11 15:01:08 $
+ * $Author: maven $
  *
  * Licence Information
  * Copyright 2004 The Concord Consortium 
