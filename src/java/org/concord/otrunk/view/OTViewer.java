bb15993f717dba92bb702f8582e91db563d5423d
maven <maven@6e01202a-0783-4428-890a-84243c50cc2b>
2005-08-03 20:52:23 +0000

    Adding licenses
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@4978 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/OTViewer.java
+++ src/java/org/concord/otrunk/view/OTViewer.java
@@ -1,7 +1,6 @@
-
 /*
  *  Copyright (C) 2004  The Concord Consortium, Inc.,
- *  10 Concord Crossing, Concord, MA 01741
+ *  10 Concord Crossing, Concord, MA 01742
  *
  *  Web Site: http://www.concord.org
  *  Email: info@concord.org
@@ -20,13 +19,13 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- */
+ * END LICENSE */
 
 /*
  * Last modification information:
- * $Revision: 1.24 $
- * $Date: 2005-08-01 14:27:33 $
- * $Author: swang $
+ * $Revision: 1.25 $
+ * $Date: 2005-08-03 20:52:23 $
+ * $Author: maven $
  *
  * Licence Information
  * Copyright 2004 The Concord Consortium 
