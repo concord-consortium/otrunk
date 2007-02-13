0b1a772c4b22d72072c7664d558f6ce022858d2e
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-02-13 22:58:31 +0000

    Fixed issue with lots of extra view container objects being left around after editing the text.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@6870 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/document/AbstractOTDocumentView.java
+++ src/java/org/concord/otrunk/view/document/AbstractOTDocumentView.java
@@ -1,11 +1,14 @@
 package org.concord.otrunk.view.document;
 
+import java.util.Vector;
+
 import org.concord.framework.otrunk.view.OTFrameManager;
 import org.concord.framework.otrunk.view.OTFrameManagerAware;
 import org.concord.framework.otrunk.view.OTViewContainer;
 import org.concord.framework.otrunk.view.OTViewContainerAware;
 import org.concord.framework.otrunk.view.OTViewFactory;
 import org.concord.framework.otrunk.view.OTViewFactoryAware;
+import org.concord.otrunk.view.OTViewContainerPanel;
 
 public class AbstractOTDocumentView extends OTTextObjectView 
 	implements OTFrameManagerAware, OTViewContainerAware, 
@@ -15,6 +18,8 @@ public class AbstractOTDocumentView extends OTTextObjectView
     private OTViewFactory viewFactory = null;
 	private OTViewContainer viewContainer;
 
+	private Vector viewContainerPanels = new Vector();
+	
 	public void setFrameManager(OTFrameManager frameManager) 
 	{
 		this.frameManager = frameManager;
@@ -44,5 +49,27 @@ public class AbstractOTDocumentView extends OTTextObjectView
 	{
 		return viewFactory;
 	}
+
+	public OTViewContainerPanel createtViewContainerPanel()
+	{
+		OTViewContainerPanel viewContainerPanel = 
+        	new OTViewContainerPanel(getFrameManager());
+        viewContainerPanel.setOTViewFactory(getViewFactory());
+        viewContainerPanel.setAutoRequestFocus(false);
+        viewContainerPanel.setUseScrollPane(false);
+        viewContainerPanel.setOpaque(false);
+        
+        viewContainerPanels.add(viewContainerPanel);
+        return viewContainerPanel;
+	}
 	
+	public void removeAllSubViews()
+	{
+		for(int i=0; i<viewContainerPanels.size(); i++){
+			OTViewContainerPanel panel = 
+				(OTViewContainerPanel) viewContainerPanels.get(i);
+			panel.setCurrentObject(null);
+		}
+		viewContainerPanels.removeAllElements();
+	}
 }
