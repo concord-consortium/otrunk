9f0907413c87aa1330d95ab1c9d7277fec377398
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-05-01 20:29:23 +0000

    Added OTJComponentViewContext and OTJComponentViewContextAware, these interfaces can be used so a view on on a page can access other views on the same page.
    Added a new method to OTJComponentViewService so more classes can use it to actually call getComponent.  This way the OTJComponentViewContext will work better.
    
    
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@7451 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/test/java/org/concord/otrunk/test/OTViewContextButtonTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.concord.otrunk.test;
+
+import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.OTObjectInterface;
+
+/**
+ * @author scott
+ *
+ */
+public interface OTViewContextButtonTest
+    extends OTObjectInterface
+{
+	public OTObject getTarget();
+}
