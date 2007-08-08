49d0e94af08490db962bd77133cfdf34b5377953
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2007-08-08 14:44:11 +0000

    blocks of css text can be added to the viewConfig
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8029 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/view/document/OTCssText.java
@@ -0,0 +1,10 @@
+package org.concord.otrunk.view.document;
+
+import org.concord.framework.otrunk.OTObjectInterface;
+
+public interface OTCssText
+    extends OTObjectInterface
+{
+	public String getCssText();
+	public void setCssText(String text);
+}
