907952767b2992010fbd8eb5231c4d61c3f32955
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-08-10 19:08:51 +0000

    Add the main class launcher object to make it easy to startup an any example main classes with an otml file.  And added an id to the toString of OTObjects.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8038 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/util/OTMainClassLauncherView.java
@@ -0,0 +1,81 @@
+package org.concord.otrunk.util;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.view.AbstractOTJComponentView;
+
+public class OTMainClassLauncherView extends AbstractOTJComponentView
+{
+	OTMainClassLauncher launcher;
+	
+	public JComponent getComponent(OTObject otObject, boolean editable)
+	{
+		launcher = (OTMainClassLauncher) otObject;
+		
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run()
+			{
+				runMainMethod();
+			}
+		});
+		
+		// Return a label that hides the main frame
+		return new JLabel("Closing..."){
+			/**
+             * 
+             */
+            private static final long serialVersionUID = 1L;
+
+			public void addNotify()
+			{
+			    // TODO Auto-generated method stub
+			    super.addNotify();
+			    
+				getTopLevelAncestor().setVisible(false);
+			}
+		};
+	}
+
+	public void runMainMethod()
+	{
+		// hide the current frame and show
+		// run the main method of the class passed in.
+		String mainClassStr = launcher.getMainClass();
+		
+		try {
+			Class mainClass = Class.forName(mainClassStr);
+			java.lang.reflect.Method mainMethod = mainClass.getMethod("main", new Class [] {String[].class});
+			mainMethod.invoke(null, new Object []{null});
+        } catch (SecurityException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (IllegalAccessException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (InvocationTargetException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        }		
+	}
+	
+	public void viewClosed()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
