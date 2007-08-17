2759e6996c3f3a6797a7699f07d35f63764910e1
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-08-17 13:19:19 +0000

    Added code to handling updating list properties.  It just copies the entries from the src list into an emptied dest list.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8057 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/prototype/OTPrototypeEventMappingHelper.java
+++ src/java/org/concord/otrunk/view/prototype/OTPrototypeEventMappingHelper.java
@@ -8,6 +8,7 @@ import org.concord.framework.otrunk.OTChangeListener;
 import org.concord.framework.otrunk.OTChangeNotifying;
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectList;
+import org.concord.framework.otrunk.OTObjectMap;
 import org.concord.otrunk.OTrunkUtil;
 
 /**
@@ -130,12 +131,27 @@ public class OTPrototypeEventMappingHelper
 			if(destListener != null) {
 				((OTChangeNotifying)dest).removeOTChangeListener(destListener);
 			}
-			OTrunkUtil.setPropertyValue(destProperty, dest, propertyValue);
+
+			if(propertyValue instanceof OTObjectList){
+				OTObjectList srcList = (OTObjectList) propertyValue;
+				OTObjectList destList = 
+					(OTObjectList) OTrunkUtil.getPropertyValue(destProperty, dest);
+				destList.removeAll();
+				for(int i=0; i<srcList.size(); i++){
+					destList.add(srcList.get(i));
+				}					
+			} else if(propertyValue instanceof OTObjectMap){
+				throw new UnsupportedOperationException("maps aren't handled yet");
+			} else {
+				OTrunkUtil.setPropertyValue(destProperty, dest, propertyValue);
+			}
 			if(destListener != null){
 				((OTChangeNotifying)dest).addOTChangeListener(destListener);
 			}
 		} catch (NoSuchMethodException e1) {
 			// TODO Auto-generated catch block
+			System.err.println("Cannot update property: " + destProperty + " on object: " + dest);
+			System.err.println("   from property: " + srcProperty + " on object: " + src);
 			e1.printStackTrace();
 		}
 
