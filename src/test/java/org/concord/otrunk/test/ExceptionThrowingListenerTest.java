4bc16cf5f7e25290a08f51a73bb44b7f0fae3d2e
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-13 23:39:14 +0000

    for scott
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8444 6e01202a-0783-4428-890a-84243c50cc2b

--- src/test/java/org/concord/otrunk/test/ExceptionThrowingListenerTest.java
+++ src/test/java/org/concord/otrunk/test/ExceptionThrowingListenerTest.java
@@ -1,7 +1,12 @@
 package org.concord.otrunk.test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.concord.framework.otrunk.OTChangeEvent;
 import org.concord.framework.otrunk.OTChangeListener;
@@ -9,34 +14,43 @@ import org.concord.framework.otrunk.OTChangeNotifying;
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.view.AbstractOTJComponentView;
 
-public class ExceptionThrowingListenerTest
-	extends AbstractOTJComponentView
+public class ExceptionThrowingListenerTest extends AbstractOTJComponentView 
+	implements OTChangeListener, ActionListener
 {
-
+	protected OTBasicTestObject otObject;
+	
 	public JComponent getComponent(OTObject otObject)
     {
-		OTChangeListener myListener = new OTChangeListener()
-		{
-			public void stateChanged(OTChangeEvent e)
-            {
-				// try casting e to something it isn't
-				Object obj = new Object();
-				System.out.println(((String)obj));
-//				throw new NullPointerException();	            
-            }			
-		};
+		this.otObject = (OTBasicTestObject)otObject;
 		
-		((OTChangeNotifying)otObject).addOTChangeListener(myListener);
+		JPanel panel = new JPanel();
+		JButton button = new JButton("click here");
+		button.addActionListener(this);
+		panel.add(button);
 		
-		((OTBasicTestObject)otObject).setString("new string");
-
-		return new JLabel("Test View");
+		((OTChangeNotifying)otObject).addOTChangeListener(this);
+		
+		return panel;
     }
 
 	public void viewClosed()
     {
 	    // TODO Auto-generated method stub
-	    
     }
 	
+	public void stateChanged(OTChangeEvent e)
+    {
+		// try casting e to something it isn't
+		Object obj = new Object();
+		System.out.println(((String)obj));
+//		throw new NullPointerException();	            
+    }
+
+	/**
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+		otObject.setString("new string");
+    }			
 }
