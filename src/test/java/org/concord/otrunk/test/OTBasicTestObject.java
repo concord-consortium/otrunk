5ae3620bb7e738ac348d361cfc1a6f6c5e679257
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-13 22:51:21 +0000

    Added a more accurate test of the change event exceptions.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8442 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/test/java/org/concord/otrunk/test/OTBasicTestObject.java
@@ -0,0 +1,10 @@
+package org.concord.otrunk.test;
+
+import org.concord.framework.otrunk.OTObjectInterface;
+
+public interface OTBasicTestObject
+    extends OTObjectInterface
+{
+	public void setString(String str);
+	public String getString();
+}
