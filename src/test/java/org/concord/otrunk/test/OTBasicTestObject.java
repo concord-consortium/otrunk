eeb9fd430416547e46783ac3a802b6483e08d6b7
imoncada <imoncada@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-14 05:38:59 +0000

    test for scott
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8452 6e01202a-0783-4428-890a-84243c50cc2b

--- src/test/java/org/concord/otrunk/test/OTBasicTestObject.java
+++ src/test/java/org/concord/otrunk/test/OTBasicTestObject.java
@@ -5,6 +5,11 @@ import org.concord.framework.otrunk.OTObjectInterface;
 public interface OTBasicTestObject
     extends OTObjectInterface
 {
+	public static String DEFAULT_string = "";
 	public void setString(String str);
 	public String getString();
+	
+	public static float DEFAULT_value = 0;
+	public void setValue(float str);
+	public float getValue();
 }
