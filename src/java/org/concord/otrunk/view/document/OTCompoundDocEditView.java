e0a4e1ce785f00116b8009bfe54216c5c3bf4899
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-17 17:52:32 +0000

    Add public pop-up editor method to allow external classes (such as the buttons in the scaffolding question editing view) to pop-up the editor
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8494 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/document/OTCompoundDocEditView.java
+++ src/java/org/concord/otrunk/view/document/OTCompoundDocEditView.java
@@ -1,12 +1,12 @@
 /*
  * Last modification information:
- * $Revision: 1.26 $
- * $Date: 2007-10-16 18:52:23 $
+ * $Revision: 1.27 $
+ * $Date: 2007-10-17 17:52:32 $
  * $Author: sfentress $
  *
  * Licence Information
  * Copyright 2007 The Concord Consortium 
-*/
+ */
 package org.concord.otrunk.view.document;
 
 import java.awt.BorderLayout;
@@ -37,25 +37,31 @@ import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 /**
- * OTCompoundDocEditView
- * This is an edit view for the compound document
- * It has a split pane with a preview in the bottoms
- * and a text area at the top for editing the code of the document 
- *
+ * OTCompoundDocEditView This is an edit view for the compound document It has a
+ * split pane with a preview in the bottoms and a text area at the top for
+ * editing the code of the document
+ * 
  * Date created: Feb 5, 2007
- *
- * @author Ingrid Moncada<p>
- *
+ * 
+ * @author Ingrid Moncada
+ *         <p>
+ * 
  */
 public class OTCompoundDocEditView extends AbstractOTDocumentView
-	implements OTViewEntryAware, ActionListener
+    implements OTViewEntryAware, ActionListener
 {
 	protected JPanel textPanel;
+
 	protected OTDocumentView previewView;
+
 	protected OTObjectEditViewConfig viewEntry;
+
 	protected OTObject document;
+
 	private JComponent editTextPane;
-	
+
+	private JPanel editPane;
+
 	/**
 	 * 
 	 */
@@ -67,106 +73,121 @@ public class OTCompoundDocEditView extends AbstractOTDocumentView
 	public JComponent getComponent(OTObject otObject)
 	{
 		document = otObject;
-		((OTChangeNotifying)document).addOTChangeListener(this);
-		
-		//Get the OTDocumentView view as the 'hardcoded' preview  
-		// this uses the NO_VIEW_MODE so the returned OTDocumentView doesn't get replaced by 
+		((OTChangeNotifying) document).addOTChangeListener(this);
+
+		// Get the OTDocumentView view as the 'hardcoded' preview
+		// this uses the NO_VIEW_MODE so the returned OTDocumentView doesn't get
+		// replaced by
 		// the edit view if the default mode is set to something else.
-		previewView = (OTDocumentView)getViewFactory().getView(document, OTDocumentView.class, OTViewFactory.NO_VIEW_MODE);
+		previewView = (OTDocumentView) getViewFactory().getView(document,
+		        OTDocumentView.class, OTViewFactory.NO_VIEW_MODE);
 
 		// no view container exists...
-	//	MyViewContainer previewViewContainer = (MyViewContainer)previewView.getViewContainer();
-	//	previewViewContainer.setParentContainer(getViewContainer());
-		
-		//set mode of OTDocumentView to mode set for OTObjectEditViewConfig, so objects
-		//in the Doc will be in the correct mode
+		// MyViewContainer previewViewContainer =
+		// (MyViewContainer)previewView.getViewContainer();
+		// previewViewContainer.setParentContainer(getViewContainer());
+
+		// set mode of OTDocumentView to mode set for OTObjectEditViewConfig, so
+		// objects
+		// in the Doc will be in the correct mode
 		previewView.setViewMode(viewEntry.getMode());
-				
-		//Create a split pane with the preview pane and the text area 
+
+		// Create a split pane with the preview pane and the text area
 		editTextPane = super.getComponent(document);
-		final JPanel editPane = createTextPanel();
-		
-		//If preview is not available, don't use it then
-		if (previewView == null){
+		editPane = createTextPanel();
+
+		// If preview is not available, don't use it then
+		if (previewView == null) {
 			return editPane;
-		} else if (viewEntry.getUsePopupEditWindows()){
+		} else if (viewEntry.getUsePopupEditWindows()) {
 			JComponent previewPane = previewView.getComponent(document);
-			
-			previewPane.addMouseListener(new MouseListener(){
+
+			previewPane.addMouseListener(new MouseListener() {
 
 				public void mouseClicked(MouseEvent evt)
-                {
-					if (!MouseEvent.getMouseModifiersText(evt.getModifiers()).equalsIgnoreCase("Button1")){
-						JFrame frame = new JFrame();
-		                frame.getContentPane().add(editPane);
-		                frame.pack();
-		                frame.setVisible(true);
+				{
+					if (!MouseEvent.getMouseModifiersText(evt.getModifiers())
+					        .equalsIgnoreCase("Button1")) {
+						showPopupEditWindow();
 					}
-                }
+				}
 
 				public void mouseEntered(MouseEvent arg0)
-                {
-	                // TODO Auto-generated method stub
-	                
-                }
+				{
+					// TODO Auto-generated method stub
+
+				}
 
 				public void mouseExited(MouseEvent arg0)
-                {
-	                // TODO Auto-generated method stub
-	                
-                }
+				{
+					// TODO Auto-generated method stub
+
+				}
 
 				public void mousePressed(MouseEvent arg0)
-                {
-	                // TODO Auto-generated method stub
-	                
-                }
+				{
+					// TODO Auto-generated method stub
+
+				}
 
 				public void mouseReleased(MouseEvent arg0)
-                {
-	                // TODO Auto-generated method stub
-	                
-                }});
+				{
+					// TODO Auto-generated method stub
+
+				}
+			});
 			return previewPane;
-		} else{
+		} else {
 			JComponent previewPane = previewView.getComponent(document);
-			
+
 			JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
-			                           editPane, previewPane);
+			        editPane, previewPane);
 			splitPane.setOneTouchExpandable(true);
 			splitPane.setDividerLocation(150);
-	
+
 			return splitPane;
 		}
 	}
-	
+
+	public void showPopupEditWindow()
+	{
+		if (viewEntry.getUsePopupEditWindows() && editPane != null) {
+			JFrame frame = new JFrame();
+			frame.getContentPane().add(editPane);
+			frame.pack();
+			frame.setVisible(true);
+		} else {
+			System.err.println("Popup editing not supported");
+		}
+	}
+
 	/**
-	 * Sets up the top panel that allows to edit the text and 
-	 * also has the buttons to insert objects
+	 * Sets up the top panel that allows to edit the text and also has the
+	 * buttons to insert objects
 	 */
 	public JPanel createTextPanel()
 	{
 		textPanel = new JPanel();
 		textPanel.setLayout(new BorderLayout());
-		
-		//Add the text edit pane at the top
+
+		// Add the text edit pane at the top
 		textPanel.add(editTextPane);
-		
-		//Create buttons and add to the bottom
+
+		// Create buttons and add to the bottom
 		JPanel buttonsPanel = new JPanel();
 
 		JButton addObjectButton = new JButton("Insert Object");
 		buttonsPanel.add(addObjectButton);
 		addObjectButton.setActionCommand("insertObject");
 		addObjectButton.addActionListener(this);
-		
+
 		JButton updatePreviewButton = new JButton("Update Preview");
 		buttonsPanel.add(updatePreviewButton);
 		updatePreviewButton.setActionCommand("updatePreview");
 		updatePreviewButton.addActionListener(this);
-		
+
 		textPanel.add(buttonsPanel, BorderLayout.NORTH);
-		
+
 		return textPanel;
 	}
 
@@ -177,56 +198,60 @@ public class OTCompoundDocEditView extends AbstractOTDocumentView
 	 */
 	public void actionPerformed(ActionEvent e)
 	{
-		if (e.getActionCommand().equals("insertObject")){
-			
-			if (textArea == null){
-				System.err.println("Error, CompoundDocEditPane has no text area to use. Should I use the compound doc directly?");
+		if (e.getActionCommand().equals("insertObject")) {
+
+			if (textArea == null) {
+				System.err
+				        .println("Error, CompoundDocEditPane has no text area to use. Should I use the compound doc directly?");
 				return;
 			}
-			
+
 			OTObject objToInsert = getObjectToInsertFromUser();
-			
-			if (objToInsert == null){
-				//No object to insert. Either we couldn't find one, or the user changed his mind
+
+			if (objToInsert == null) {
+				// No object to insert. Either we couldn't find one, or the user
+				// changed his mind
 				return;
 			}
-			
+
 			OTObjectService objectService = document.getOTObjectService();
 			String strObjID = objectService.getExternalID(objToInsert);
-			
+
 			String paragraphBreak = "";
-			if (viewEntry.getAddParagraphAfterObject()){
+			if (viewEntry.getAddParagraphAfterObject()) {
 				paragraphBreak = "<p/>";
 			}
-			String strObjText = "<object refid=\"" + strObjID + "\" />" + paragraphBreak;
-			
+			String strObjText = "<object refid=\"" + strObjID + "\" />"
+			        + paragraphBreak;
+
 			int pos = textArea.getSelectionStart();
 			textArea.insert(strObjText, pos);
-			
+
 			updatePreviewView();
-			
-		}
-		else if (e.getActionCommand().equals("updatePreview")){
+
+		} else if (e.getActionCommand().equals("updatePreview")) {
 			boolean textHasErrors = false;
 			SAXBuilder builder = new SAXBuilder();
 			try {
-				String documentText = "<root>" + ((OTCompoundDoc)document).getBodyText() + "</root>";				
-	        	StringReader reader = new StringReader(documentText);
-	            builder.build(reader);
-            } catch (JDOMException e1) {
-            	String warning = "Warning: There is an HTML error in your text.\n " + e1.getLocalizedMessage();
-            	JOptionPane.showMessageDialog(textPanel, warning,
-            		    "HTML error",
-            		    JOptionPane.ERROR_MESSAGE);
-            	System.err.println("User-generated XML error: "+e1.getCause());
-            	textHasErrors = true;
-            } catch (IOException e1) {
-	            e1.printStackTrace();
-            }
-            
-            if (!textHasErrors){
-            	updatePreviewView();
-            }
+				String documentText = "<root>"
+				        + ((OTCompoundDoc) document).getBodyText() + "</root>";
+				StringReader reader = new StringReader(documentText);
+				builder.build(reader);
+			} catch (JDOMException e1) {
+				String warning = "Warning: There is an HTML error in your text.\n "
+				        + e1.getLocalizedMessage();
+				JOptionPane.showMessageDialog(textPanel, warning, "HTML error",
+				        JOptionPane.ERROR_MESSAGE);
+				System.err
+				        .println("User-generated XML error: " + e1.getCause());
+				textHasErrors = true;
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+			if (!textHasErrors) {
+				updatePreviewView();
+			}
 		}
 	}
 
@@ -235,22 +260,27 @@ public class OTCompoundDocEditView extends AbstractOTDocumentView
 	 */
 	public void updatePreviewView()
 	{
-		if (previewView == null) return;
+		if (previewView == null)
+			return;
 		previewView.updateFormatedView();
 	}
 
 	/**
-	 * Shows a dialog with the list of possible objects to insert and lets the user choose
+	 * Shows a dialog with the list of possible objects to insert and lets the
+	 * user choose
 	 * 
 	 * @return OT Object selected by the user
 	 */
 	private OTObject getObjectToInsertFromUser()
 	{
 		OTObject otObj = null;
-		
-		otObj = OTObjectListViewer.showDialog(textPanel, "Choose object to add", getFrameManager(), getViewFactory(), 
-				viewEntry.getObjectsToInsert(), ((OTCompoundDoc)pfObject).getOTObjectService(), true, viewEntry.getCopyNewObjectsByDefault());
-		
+
+		otObj = OTObjectListViewer.showDialog(textPanel,
+		        "Choose object to add", getFrameManager(), getViewFactory(),
+		        viewEntry.getObjectsToInsert(), ((OTCompoundDoc) pfObject)
+		                .getOTObjectService(), true, viewEntry
+		                .getCopyNewObjectsByDefault());
+
 		return otObj;
 	}
 
@@ -259,44 +289,48 @@ public class OTCompoundDocEditView extends AbstractOTDocumentView
 	 */
 	public void setViewEntry(OTViewEntry viewEntry)
 	{
-		this.viewEntry = (OTObjectEditViewConfig)viewEntry;
+		this.viewEntry = (OTObjectEditViewConfig) viewEntry;
 	}
-	
+
 	public OTObjectEditViewConfig getViewEntry()
 	{
 		return viewEntry;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.concord.otrunk.view.document.AbstractOTDocumentView#viewClosed()
 	 */
-	public void viewClosed() 
+	public void viewClosed()
 	{
 		super.viewClosed();
-		
-		if(previewView != null){
+
+		if (previewView != null) {
 			previewView.viewClosed();
 		}
 	}
-	
-	public void stateChanged(OTChangeEvent e){
 
-        if(isChangingText()){
-            // we have caused this event ourselves
-            return;
-        }
-        
-        if(labelView != null) {
-            labelView.setText(pfObject.getDocumentText());
-            return;
-        }
-        
-        try {
-            textAreaModel.replace(0, textAreaModel.getLength(), pfObject.getDocumentText(), null);
-        } catch (Exception exc) {
-         //   exc.printStackTrace();
-        }
-        updatePreviewView();
-        
+	public void stateChanged(OTChangeEvent e)
+	{
+
+		if (isChangingText()) {
+			// we have caused this event ourselves
+			return;
+		}
+
+		if (labelView != null) {
+			labelView.setText(pfObject.getDocumentText());
+			return;
+		}
+
+		try {
+			textAreaModel.replace(0, textAreaModel.getLength(), pfObject
+			        .getDocumentText(), null);
+		} catch (Exception exc) {
+			// exc.printStackTrace();
+		}
+		updatePreviewView();
+
 	}
 }
