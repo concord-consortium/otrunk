b8b302f342df2eb7a63f5494868c384cd8353351
skim <skim@6e01202a-0783-4428-890a-84243c50cc2b>
2007-10-17 14:00:03 +0000

    Added getSrc method to OTCssText.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@8484 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/document/OTCssText.java
+++ src/java/org/concord/otrunk/view/document/OTCssText.java
@@ -1,10 +1,12 @@
 package org.concord.otrunk.view.document;
 
+import java.net.URL;
 import org.concord.framework.otrunk.OTObjectInterface;
 
 public interface OTCssText
     extends OTObjectInterface
 {
 	public String getCssText();
+	public URL getSrc();
 	public void setCssText(String text);
 }
