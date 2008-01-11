6fc0ca5e497ff3db645617472da049241009c8a0
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2008-01-11 20:10:51 +0000

    Use the URL class to handle appending the codebase to the link url.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@9271 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/document/OTDocumentView.java
+++ src/java/org/concord/otrunk/view/document/OTDocumentView.java
@@ -445,20 +445,9 @@ public class OTDocumentView extends AbstractOTDocumentView implements
 						return;
 					}
 					
-					URL newTarget = null;
-					// if the link appears absolute
-					// then construct the url using only the host information (or file:// for local files)
-					if (linkTarget.charAt(0) == '/') {
-						// use just the protocol, port, host info from the codebase
-						if (codebase.getProtocol().startsWith("http")) {
-							newTarget = new URL(codebase.toExternalForm().substring(0,codebase.toExternalForm().indexOf('/', 8)) + linkTarget);
-						} else if (codebase.getProtocol().startsWith("file")) {
-							newTarget = new URL(codebase.getProtocol() + "://" + linkTarget);
-						}
-					} else {
-						// since the codebase url has the otml file at the end, strip it and append the relative url
-						newTarget = new URL(codebase.toExternalForm().substring(0,codebase.toExternalForm().lastIndexOf("/")+1) + linkTarget);
-					}
+					// construct a new URL using the document's codebase as a base
+					URL newTarget = new URL(codebase, linkTarget);
+					
 					if (newTarget != null) {
 						// try to launch the url in a browser window
     					OTExternalAppService extAppService = 
