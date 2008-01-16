c3e7fd60d2aa9abd4375f2f8c468db20fec9fcf3
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-01-16 18:15:32 +0000

    OTIncludeRootObject and associated view can be used to both import an external otml file and use its root object at the same time.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@9306 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/OTIncludeRootObject.java
@@ -0,0 +1,10 @@
+package org.concord.otrunk;
+
+import java.net.URL;
+
+import org.concord.framework.otrunk.OTObjectInterface;
+
+public interface OTIncludeRootObject extends OTObjectInterface 
+{
+	public URL getHref();
+}
