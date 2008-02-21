2935f022b42eda78cd1c69bba65ac03fd0dfade5
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-02-21 18:41:04 +0000

    New extension of JEditorPane should fix copy-paste bug
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@12525 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/view/document/edit/MyJEditorPane.java
@@ -0,0 +1,34 @@
+package org.concord.otrunk.view.document.edit;
+
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.util.logging.Level;
+
+import javax.swing.JEditorPane;
+
+public class MyJEditorPane extends JEditorPane
+{
+	public void paste() {
+        Clipboard clipboard = getToolkit().getSystemClipboard();
+        Transferable content = clipboard.getContents(this);
+        if (content != null) {
+            DataFlavor[] flavors = content.getTransferDataFlavors();
+            try {
+                for (int i = 0; i < flavors.length; i++) {
+                    if (String.class.equals(flavors[i].getRepresentationClass())) {
+                        Object data = content.getTransferData(flavors[i]);
+
+                        if (flavors[i].isMimeTypeEqual("text/plain")) {
+                            // This works but we lose all the formatting.
+                            replaceSelection(data.toString());
+                            break;
+                        } 
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
