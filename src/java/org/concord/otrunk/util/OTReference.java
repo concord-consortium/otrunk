e957f71f045d5e185a81b8ca623641fbfffd8ef1
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-06-09 16:08:53 +0000

    An OTReference object can be used to store meta-information about a link between two objects.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@13776 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/util/OTReference.java
@@ -0,0 +1,30 @@
+package org.concord.otrunk.util;
+
+import org.concord.framework.otrunk.OTObject;
+
+/**
+ * An OTReference object can be used to store meta-information about a link
+ * between two objects. In our current pattern, an OTReference is contained by a parent 
+ * object, the "FROM" object, and so this is not explicitly set. The "TO" object is 
+ * explicitly set in the OTReference.
+ * 
+ * @author sfentress
+ *
+ */
+public interface OTReference
+{
+	public OTObject getReferencedObject();
+	public void setReferencedObject(OTObject referencedObject);
+	
+	/**
+	 * A description of the type of link. Possible annotations include:
+	 * 
+	 * * CONTEXT				// e.g. the model that a question is asking about
+	 * * ACTIVITY-REFERENCE		// the activity an object relates to
+	 * * ...
+	 * 
+	 * @return
+	 */
+	public String getAnnotation();
+	public void setAnnotation(String annotation);
+}
