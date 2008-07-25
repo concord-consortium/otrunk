f4965b6c83a3630117b46add960d4a30b6d2649b
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2008-07-25 03:29:45 +0000

    Modified OTIncludeRootObject so it can more easily be used without its view. Updated multiuser root object so it can take an OTIncludeRootObject.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@14414 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/OTIncludeRootObject.java
+++ src/java/org/concord/otrunk/OTIncludeRootObject.java
@@ -2,9 +2,46 @@ package org.concord.otrunk;
 
 import java.net.URL;
 
-import org.concord.framework.otrunk.OTObjectInterface;
+import org.concord.framework.otrunk.DefaultOTObject;
+import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.OTResourceSchema;
+import org.concord.framework.otrunk.OTrunk;
 
-public interface OTIncludeRootObject extends OTObjectInterface 
+public class OTIncludeRootObject extends DefaultOTObject 
 {
-	public URL getHref();
+	public static interface ResourceSchema extends OTResourceSchema
+	{
+		public URL getHref();
+	}
+	protected ResourceSchema resources;
+
+	
+	public OTIncludeRootObject(ResourceSchema resources)
+    {
+	    super(resources);
+	    this.resources = resources;
+    }
+
+	public URL getHref()
+	{
+		return resources.getHref();
+	}
+	
+	public OTObject getReference()
+	{
+		URL url = getHref();
+		
+		OTrunkImpl otrunkImpl = (OTrunkImpl)getOTObjectService().getOTrunkService(OTrunk.class);
+		
+		try {
+			OTObject refObject = otrunkImpl.getExternalObject(url, getOTObjectService());			
+			return refObject;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
 }
