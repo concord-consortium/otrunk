432575c3863c8059d562d52f4685910f334e7225
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2008-12-05 17:09:33 +0000

    Add documentListEditView and documentTitleEditView, to be used in a template authoring system such as RITES
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@16467 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/org/concord/otrunk/view/document/edit/OTDocumentTitleEditView.java
@@ -0,0 +1,46 @@
+package org.concord.otrunk.view.document.edit;
+
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import org.concord.framework.otrunk.OTChangeListener;
+import org.concord.framework.otrunk.OTChangeNotifying;
+import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.view.AbstractOTJComponentView;
+import org.concord.otrunk.view.document.OTDocument;
+
+/**
+ * The TitleEditView is a view for a document that would be a section of a larger
+ * document, to be used in a template authoring system such as RITES.
+ * 
+ * This view is simply a text area, used to edit a title of a page or section.
+ * 
+ * @author sfentress
+ *
+ */
+public class OTDocumentTitleEditView extends AbstractOTJComponentView
+{
+
+	private OTDocument document;
+
+	public JComponent getComponent(OTObject otObject)
+    {
+		this.document = (OTDocument)otObject;
+		
+		JTextField textField = new JTextField(document.getDocumentText());
+		textField.setFont(textField.getFont().deriveFont(Font.BOLD, 14));
+		
+		String name = document.getName();
+		if (name == null) name = "Title";
+		Border title = BorderFactory.createTitledBorder(name);
+		Border buffer = BorderFactory.createEmptyBorder(3, 3, 3, 3);
+		textField.setBorder(BorderFactory.createCompoundBorder(buffer, title));
+		
+	    return textField;
+    }
+
+}
