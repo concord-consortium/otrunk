f10889ef1437966f22d50212fd8382f4b15bbc05
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-14 23:27:04 +0000

    this file should have been added in the previous commit
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@16656 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/view/OTConfig.java
+++ src/java/org/concord/otrunk/view/OTConfig.java
@@ -24,6 +24,7 @@ public class OTConfig
 	public final static String REMOTE_SAVE_DATA_PROP = "otrunk.remote_save_data";
 	public final static String SHOW_DESTRUCTIVE_MENU_ITEMS_PROP = "otrunk.view.destructive_menu";	// "New" and "Open"
 	public final static String TRACE_DB_LOAD_TIME = "otrunk.db.trace.load";
+	public final static String USE_ASM = "otrunk.use.asm";
 	
 	/**
      * This is yet another hack to support something like layers or mutliple files.
