b0b9da1d4aa8305cd864b37036aff5e8d44d2727
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-22 23:31:10 +0000

    Change the workgroupmemberchooser so it doesn't directly depend on OTDocument.  This change is to facilitate moving OTDocument out of the otrunk project.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@16773 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/user/OTWorkgroupMemberChooser.java
+++ src/java/org/concord/otrunk/user/OTWorkgroupMemberChooser.java
@@ -1,9 +1,9 @@
 package org.concord.otrunk.user;
 
+import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectInterface;
 import org.concord.framework.otrunk.OTObjectList;
 import org.concord.otrunk.view.OTClassMember;
-import org.concord.otrunk.view.document.OTDocument;
 
 public interface OTWorkgroupMemberChooser
     extends OTObjectInterface
@@ -34,7 +34,10 @@ public interface OTWorkgroupMemberChooser
 	 * This Document is a template for the header text in the view. The text will be run through a simple substitution engine prior to being rendered.
 	 * Valid substitution variables are:
 	 *   #{user_name}
+	 * The type of this object is set to OTObject to remove its dependency on OTDocument which 
+	 * is no longer in the OTrunk project.  The view now casts this to OTDocument.
+	 * This class should probably be moved to another package/project.
 	 * @return
 	 */
-	public OTDocument getTextTemplate();
+	public OTObject getTextTemplate();
 }
