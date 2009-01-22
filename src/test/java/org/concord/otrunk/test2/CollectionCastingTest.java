b133a84823b57f58900566af02df02876c052b91
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-22 18:16:20 +0000

    updated the test to use OTListTestObject instead of OTDocument and added a second test to make sure an exception is thrown when toArray is used with an invalid array type.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@16756 6e01202a-0783-4428-890a-84243c50cc2b

--- src/test/java/org/concord/otrunk/test2/CollectionCastingTest.java
+++ src/test/java/org/concord/otrunk/test2/CollectionCastingTest.java
@@ -9,25 +9,44 @@ import junit.framework.TestCase;
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectList;
 import org.concord.otrunk.datamodel.OTDatabase;
+import org.concord.otrunk.test.OTListTestObject;
 import org.concord.otrunk.view.OTViewerHelper;
-import org.concord.otrunk.view.document.OTCompoundDoc;
 
 
 public class CollectionCastingTest extends TestCase
 {
-    /**
-     * Test creating an ArrayList from an OTObjectList.
-     * Used to throw an IllegalArgumentException from the constructor of ArrayList.
-     */
-    public void testArrayListFromOtObjectList() throws Exception {
+	public OTObjectList initObjectList() throws Exception
+	{
         URL url = getClass().getResource("/collection-test.otml");
         OTViewerHelper viewerHelper = new OTViewerHelper();
         OTDatabase mainDb = viewerHelper.loadOTDatabase(url);
         viewerHelper.loadOTrunk(mainDb, null);
 
-        OTCompoundDoc doc = (OTCompoundDoc) viewerHelper.getRootObject();
-        OTObjectList objectList = doc.getDocumentRefsAsObjectList();
+        OTListTestObject list = (OTListTestObject) viewerHelper.getRootObject();
+        return list.getObjectList();
+        
+	}
+	
+    /**
+     * Test creating an ArrayList from an OTObjectList.
+     * Used to throw an IllegalArgumentException from the constructor of ArrayList.
+     */
+    public void testArrayListFromOtObjectList() throws Exception {
+        OTObjectList objectList = initObjectList();
 
-        ArrayList<OTObject> aList = new ArrayList<OTObject>(objectList);
+        new ArrayList<OTObject>(objectList);
     }
+    
+    public void testToInvalidArray() throws Exception {
+        OTObjectList objectList = initObjectList();
+
+        try{
+        	objectList.toArray(new Integer[0]);
+        } catch (ArrayStoreException e){
+        	return;
+        }
+        
+        assertTrue("storing a OTObjectList in a invalid array did not throw an exception", false);
+    }
+    
 }
