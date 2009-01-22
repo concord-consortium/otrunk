8b90dd3be65010479c3b87ec9e9a3c8504722e56
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-01-22 18:16:20 +0000

    updated the test to use OTListTestObject instead of OTDocument and added a second test to make sure an exception is thrown when toArray is used with an invalid array type.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@16756 6e01202a-0783-4428-890a-84243c50cc2b

--- src/test/java/org/concord/otrunk/test/OTListTestObject.java
+++ src/test/java/org/concord/otrunk/test/OTListTestObject.java
@@ -1,10 +1,13 @@
 package org.concord.otrunk.test;
 
 import org.concord.framework.otrunk.OTObjectInterface;
+import org.concord.framework.otrunk.OTObjectList;
 import org.concord.framework.otrunk.OTResourceList;
 
 public interface OTListTestObject
     extends OTObjectInterface
 {
 	public OTResourceList getResourceList();
+	
+	public OTObjectList getObjectList();
 }
