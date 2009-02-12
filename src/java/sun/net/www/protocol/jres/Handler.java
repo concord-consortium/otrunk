81b4c2e84e217436025a4d457c81eb2af1b41f0e
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-02-12 17:47:25 +0000

    Re-adding handler
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@17093 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/java/sun/net/www/protocol/jres/Handler.java
@@ -0,0 +1,17 @@
+package sun.net.www.protocol.jres;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Handler extends java.net.URLStreamHandler
+{
+
+	@Override
+    protected URLConnection openConnection(URL u)
+        throws IOException
+    {
+	    return new JResConnection(u);
+    }
+
+}
