b818b0d929efaa4a3306ae6db2d31612687f75d0
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2009-03-05 15:52:51 +0000

    More fixes to the onlyModifications version of the Copy code.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@17441 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/datamodel/Copier.java
+++ src/java/org/concord/otrunk/datamodel/Copier.java
@@ -4,6 +4,9 @@
 package org.concord.otrunk.datamodel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,10 +25,12 @@ public class Copier
 	OTDataObject root;
 	
 	ArrayList<CopyEntry> toBeCopied;
+	ArrayList<CopyEntry> recurseOnly;
 	private OTDataList orphanList;
 	private OTExternalIDProvider idProvider;
 	private OTDataObjectFinder dataObjectFinder;
 	private boolean onlyModifications;
+	private static final Logger logger = Logger.getLogger(Copier.class.getCanonicalName());
 
     private static class CopyEntry
     {    	
@@ -47,6 +52,7 @@ public class Copier
 		this.destinationDb = destinationDb;
 		this.sourceDb = sourceDb;
 		this.toBeCopied = new ArrayList<CopyEntry>();
+		this.recurseOnly = new ArrayList<CopyEntry>();
 		this.orphanList = orphanDataList;
 		this.idProvider = idProvider;
 		this.dataObjectFinder = dataObjectFinder;
@@ -103,34 +109,52 @@ public class Copier
 					}
 				}
 				
-				OTID itemID = itemObj.getGlobalId();
-				if (itemID instanceof OTTransientMapID) {
-					itemID = ((OTTransientMapID)itemID).getMappedId();
+				int copyMaxDepth = -1;
+				
+				if(maxDepth != -1){
+					copyMaxDepth = maxDepth-1; 
 				}
-				// System.err.println("Copying object: " + itemObj.getGlobalId());
-				if (onlyModifications && itemObj instanceof CompositeDataObject && ! ((CompositeDataObject) itemObj).isModified()) {
-					// System.err.println("Not modified: only a reference");
-					// don't copy the object if it's not modified and onlyModifications is true
-					// instead, include an object reference to the original
-					itemCopyEntry = new CopyEntry(itemObj, -1, destinationDb.getOTDataObject(root, itemID));
-				} else {
-					OTDataObject itemCopy;
-					if (onlyModifications && itemObj instanceof CompositeDataObject && ((CompositeDataObject) itemObj).isModified() && (((CompositeDataObject) itemObj).getActiveDeltaObject() != null)) {
-						// System.err.println("Modified: changed copy");
-						// if the object has an active delta object, then it's only modified (not newly created) so if we request the data object from the destination db, it will handle creating the deltamap for us
-						itemCopy = destinationDb.getOTDataObject(root, itemID);
-					} else {
-						// System.err.println("Modified: full copy");
-						// the object is newly created *or* we're forcing a full copy
-						itemCopy = destinationDb.createDataObject(itemObj.getType());
-					}
-    				int copyMaxDepth = -1;
-    				
-    				if(maxDepth != -1){
-    					copyMaxDepth = maxDepth-1; 
+				
+				if (onlyModifications) {
+    				OTID itemID = itemObj.getGlobalId();
+    				if (itemID instanceof OTTransientMapID) {
+    					itemID = ((OTTransientMapID)itemID).getMappedId();
     				}
-    				itemCopyEntry = new CopyEntry(itemObj, copyMaxDepth, itemCopy);
-    				toBeCopied.add(itemCopyEntry);
+    				
+    				if (itemObj instanceof CompositeDataObject) {
+    					CompositeDataObject compositeObj = (CompositeDataObject) itemObj;
+    					OTDataObject itemCopy;
+    					boolean onlyRecurse = false;
+    					if (! compositeObj.isModified()) {
+        					logger.finer("Not modified: only a reference");
+        					// don't copy the object if it's not modified and onlyModifications is true
+        					// instead, include an object reference to the original
+        					itemCopy = destinationDb.getOTDataObject(root, itemID);
+        					onlyRecurse = true;
+    					} else if ( compositeObj.getActiveDeltaObject() != null) {
+    						logger.finer("Modified: changed copy");
+    						// if the object has an active delta object, then it's only modified (not newly created) so if we request the data object from the destination db, it will handle creating the deltamap for us
+    						itemCopy = destinationDb.getOTDataObject(root, itemID);
+    					} else {
+    						logger.finer("Modified: full copy");
+    						// the object is newly created *or* we're forcing a full copy
+    						itemCopy = destinationDb.createDataObject(itemObj.getType());
+    					}
+    					itemCopyEntry = new CopyEntry(itemObj, copyMaxDepth, itemCopy);
+        				toBeCopied.add(itemCopyEntry);
+        				if (onlyRecurse) {
+        					recurseOnly.add(itemCopyEntry);
+        				}
+    				} else {
+    					OTDataObject itemCopy =  destinationDb.createDataObject(itemObj.getType()); 
+    					itemCopyEntry = new CopyEntry(itemObj, copyMaxDepth, itemCopy); 
+    					toBeCopied.add(itemCopyEntry); 
+    				}
+    				
+				} else {
+					OTDataObject itemCopy =  destinationDb.createDataObject(itemObj.getType()); 
+					itemCopyEntry = new CopyEntry(itemObj, copyMaxDepth, itemCopy); 
+					toBeCopied.add(itemCopyEntry); 
 				}
 			} else {
 				// we should not return here, it should just use the copy's global id
@@ -161,8 +185,9 @@ public class Copier
     	int currentIndex = 0;
     	while(currentIndex < toBeCopied.size()){
     		CopyEntry entry = toBeCopied.get(currentIndex);
+    		boolean onlyRecurse = recurseOnly.contains(entry);
     		OTDataObject original = entry.original; 
-    	//	System.out.println(original+" ; "+original.getType());
+    	//	logger.fine(original+" ; "+original.getType());
     		OTDataObject copy = entry.copy;
     		if(copy == null) {
     			copy = otDb.createDataObject(original.getType());
@@ -173,47 +198,129 @@ public class Copier
     		// This is used to handle a hack for strings which reference objects
     		ArrayList<String> secondPassKeys = new ArrayList<String>();
     		
+    		logger.fine("Processing object: " + original.getGlobalId());
+    		
     		for(int i=0; i<keys.length; i++){
+    			logger.fine("Processing attribute: " + keys[i]);
     			Object resource = original.getResource(keys[i]);
+    			boolean keyModified = false;
+    			if (onlyModifications) {
+        			if (original instanceof CompositeDataObject) {
+        				if (((CompositeDataObject) original).isModified()) {
+            				OTDataObject deltaObj = ((CompositeDataObject) original).getActiveDeltaObject();
+            				if (deltaObj != null && deltaObj.containsKey(keys[i])) {
+            					logger.finer("Delta object contains key!");
+            					keyModified = true;
+            				} else if (deltaObj == null) {
+            					// composite data object without an active delta object means it was newly created
+            					logger.finer("Original is newly created! Key counts as modified!");
+            					keyModified = true;
+            				}
+            			}
+        			}
+    			}
 
     			if(resource instanceof OTDataList){
-    				OTDataList copyList =
-    					copy.getResourceCollection(keys[i], 
-                    		OTDataList.class);
     				OTDataList list = (OTDataList)resource;
-    				copyList.removeAll();
-    				for(int j=0; j<list.size(); j++){
-    					Object listItem = list.get(j);
-    					
-    					listItem = handleChild(listItem, entry.maxDepth);
-    					copyList.add(listItem);
+    				
+    				// if onlyModifications is set, only copy the list if something has changed
+    				if (onlyModifications) {
+    					boolean modifiedList = false;
+    					ArrayList<Object> tempList = new ArrayList<Object>();
+    					for(int j=0; j<list.size(); j++){
+        					Object listItem = list.get(j);
+        					listItem = handleChild(listItem, entry.maxDepth);
+        					tempList.add(listItem);
+        					if (listItem instanceof CompositeDataObject && ((CompositeDataObject) listItem).isModified()) {
+        						modifiedList = true;
+        					}
+    					}
+    					logger.finer("List is modified: " + (modifiedList || keyModified));
+    					if (modifiedList || keyModified) {
+    						// the list was modified so copy all the listItems
+    						OTDataList copyList =
+    	    					copy.getResourceCollection(keys[i], 
+    	                    		OTDataList.class);
+    	    				
+    	    				copyList.removeAll();
+    	    				for(Object o : tempList){
+    	    					copyList.add(o);
+    	    				}
+    					}
+    				} else {
+        				OTDataList copyList =
+        					copy.getResourceCollection(keys[i], 
+                        		OTDataList.class);
+        				
+        				copyList.removeAll();
+        				for(int j=0; j<list.size(); j++){
+        					Object listItem = list.get(j);
+        					
+        					listItem = handleChild(listItem, entry.maxDepth);
+        					copyList.add(listItem);
+        				}
     				}
     			} else if(resource instanceof OTDataMap){
-    				OTDataMap copyMap =
-    					copy.getResourceCollection(keys[i], 
-                    		OTDataMap.class);                    
+    				// if onlyModifications is set, only process the map if something has changed
     				OTDataMap map = (OTDataMap)resource;
-    				copyMap.removeAll();
     				String [] mapKeys = map.getKeys();
-    				for(int j=0; j<mapKeys.length; j++){
-    					Object item = map.get(mapKeys[j]);
-    					item = handleChild(item, entry.maxDepth);
-    					copyMap.put(mapKeys[j], item);
+    				if (onlyModifications) {
+    					boolean modifiedMap = false;
+    					HashMap<String, Object> tempMap = new HashMap<String, Object>();
+    					for(int j=0; j<mapKeys.length; j++){
+        					Object item = map.get(mapKeys[j]);
+        					item = handleChild(item, entry.maxDepth);
+        					tempMap.put(mapKeys[j], item);
+        					if (item instanceof CompositeDataObject && ((CompositeDataObject) item).isModified()) {
+        						modifiedMap = true;
+        					}
+    					}
+    					logger.finer("Map is modified: " + (modifiedMap || keyModified));
+    					if (modifiedMap || keyModified) {
+    						// the map was modified so copy all the listItems
+    						OTDataMap copyMap =
+    	    					copy.getResourceCollection(keys[i], 
+    	    						OTDataMap.class);
+    	    				
+    						copyMap.removeAll();
+    	    				for(Entry<String, Object> o : tempMap.entrySet()){
+    	    					copyMap.put(o.getKey(), o.getValue());
+    	    				}
+    					}
+    				} else {
+        				OTDataMap copyMap =
+        					copy.getResourceCollection(keys[i], 
+                        		OTDataMap.class);                    
+        				copyMap.removeAll();
+        				
+        				for(int j=0; j<mapKeys.length; j++){
+        					Object item = map.get(mapKeys[j]);
+        					item = handleChild(item, entry.maxDepth);
+        					copyMap.put(mapKeys[j], item);
+        				}
     				}
-    			} else if(resource instanceof OTXMLString) { 
-    				secondPassKeys.add(keys[i]);
+    			} else if(resource instanceof OTXMLString) {
+    				if (onlyModifications) {
+        				if ((! onlyRecurse) && keyModified) {
+        					secondPassKeys.add(keys[i]);
+        				}
+    				} else {
+    					secondPassKeys.add(keys[i]);
+    				}
     			} else {
     				resource = handleChild(resource, entry.maxDepth);
-    				copy.setResource(keys[i], resource);
+    				if (! onlyRecurse) {
+    					copy.setResource(keys[i], resource);
+    				}
     			}
     		}
     		
     		for(int i=0; i<secondPassKeys.size(); i++){
-    			System.out.println("loop secondpass");
+    			logger.finer("loop secondpass");
     			String key = secondPassKeys.get(i);
     			Object resource = original.getResource(key);
 
-    			System.out.println("text copying " + key);
+    			logger.finer("text copying " + key);
 
     			// This is a hack, if we had namespaces it wouldn't be as
 				// much of a hack.
@@ -272,7 +379,7 @@ public class Copier
 				if(matchedDataObject == null){
 					// this id isn't in our database or it is invalid
 					// print a warning and continue
-					System.err.println("Can't find object to copy: " + otid + " skipping object");
+					logger.warning("Can't find object to copy: " + otid + " skipping object");
 					
 					// put the same string back in. 
 					matcher.appendReplacement(copiedStringBuf, "$0");
@@ -289,11 +396,12 @@ public class Copier
 				
 				
 				if(orphanList == null){
-					System.err.println("Cannot copy objects referenced from " +
-					"xml strings, that are not stored somewhere else");
-					System.err.println("  original object: " + otid);
-					System.err.println("  object referencing it: " + original);
-					System.err.println("  string property: " + key);
+					String msg = "Cannot copy objects referenced from " +
+					"xml strings, that are not stored somewhere else";
+					msg += "\n" + "  original object: " + otid;
+					msg += "\n" + "  object referencing it: " + original;
+					msg += "\n" + "  string property: " + key;
+					logger.warning(msg);
 					
 					// Just use the original id
 					copiedId = otid;							
