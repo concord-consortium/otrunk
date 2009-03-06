457f625d508dd9a0a56603eef5c1aa507edf8899
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2009-03-06 19:38:01 +0000

    Didn't mean to check in changes to ExporterJDOM.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@17464 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/xml/ExporterJDOM.java
+++ src/java/org/concord/otrunk/xml/ExporterJDOM.java
@@ -582,15 +582,6 @@ public class ExporterJDOM
 			// in that case it should be written out anyhow.  I'm not sure how to 
 			// do that yet.
 			ArrayList<OTDataObject> incomingReferences = incomingReferenceMap.get(id);
-			if (parentResourceName != null && parentResourceName.equals("nonDeltaObjects")) {
-				if (incomingReferences == null || incomingReferences.size() <= 1) {
-					// FIXME if we're in a non-delta map, skip rendering the object altogether
-					objectEl.setAttribute("toBeRemoved", "true");
-					// for (Object att : objectEl.getAttributes()) {
-					// 	objectEl.removeAttribute((Attribute) att);
-					// }
-				}
-			}
 			if((xmlDO != null && xmlDO.isPreserveUUID() && id instanceof OTUUID) ||
 					(incomingReferences != null && incomingReferences.size() > 1)){
 				objectEl.setAttribute("id", id.toExternalForm());
