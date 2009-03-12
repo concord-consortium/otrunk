72234a0d25a9ca69ef493b8ace508b4b5e016040
sbannasch <sbannasch@6e01202a-0783-4428-890a-84243c50cc2b>
2009-03-12 04:07:31 +0000

    collect_imports.rb can now be run directly
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@17526 6e01202a-0783-4428-890a-84243c50cc2b

--- schema/collect_imports.rb
+++ schema/collect_imports.rb
@@ -1,9 +1,17 @@
-Dir.chdir ARGV[0] unless ARGV.empty?
-$stderr.puts "1) parsing all otml files located in this dir tree: #{ARGV[0]},"
-$stderr.puts "2) extracting all <import> elements"
-$stderr.puts "3) writing unique set to: /tmp/all-otrunk.xml"
-File.open('/tmp/all-otrunk.xml','w') do |f| 
-  f.write "<otrunk>\n"
-  f.write `find . -name "*.otml" -print0 | xargs -0  egrep -h -o "<import class=.*" | sed 's/ *\\/>.*/\\/>/' | sort | uniq`
-  f.write "</otrunk>\n"
+if ARGV.empty?
+  current_dir = File.dirname(File.expand_path(__FILE__))
+  otrunk_examples = current_dir + '/../../otrunk-examples'
+else
+  otrunk_examples = ARGV[0]
+end
+
+Dir.chdir(otrunk_examples) do
+  $stderr.puts "1) parsing all otml files located in this dir tree: #{ARGV[0]},"
+  $stderr.puts "2) extracting all <import> elements"
+  $stderr.puts "3) writing unique set to: /tmp/all-otrunk.xml"
+  File.open('/tmp/all-otrunk.xml','w') do |f| 
+    f.write "<otrunk>\n"
+    f.write `find . -name "*.otml" -print0 | xargs -0  egrep -h -o "<import class=.*" | sed 's/ *\\/>.*/\\/>/' | sort | uniq`
+    f.write "</otrunk>\n"
+  end
 end
