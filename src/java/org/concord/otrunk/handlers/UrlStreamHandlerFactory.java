749d6efaff49064e52ddd29ead078d5fd143d212
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2009-04-17 20:35:18 +0000

    Reformated the file, and changed the system messages to use the logging system.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@18276 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/handlers/UrlStreamHandlerFactory.java
+++ src/java/org/concord/otrunk/handlers/UrlStreamHandlerFactory.java
@@ -7,81 +7,85 @@ package org.concord.otrunk.handlers;
  * 
  * No license is given, but owner gives his consent for use in the last link above. If we end up keeping this, we should contact owner for license.
  */
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 // not yet tested...
 public class UrlStreamHandlerFactory implements URLStreamHandlerFactory {
+	private static final Logger logger =
+		Logger.getLogger(UrlStreamHandlerFactory.class.getCanonicalName());
 
-  public static final String JAVA_PROTOCOL_HANDLER_PKGS = "java.protocol.handler.pkgs";
+	public static final String JAVA_PROTOCOL_HANDLER_PKGS = "java.protocol.handler.pkgs";
 
-  private static UrlStreamHandlerFactory instance = null;
+	private static UrlStreamHandlerFactory instance = null;
 
-  private HashMap handlerClasses = new HashMap();
+	private HashMap handlerClasses = new HashMap();
 
-  private UrlStreamHandlerFactory() {
-    super();
-  }
+	private UrlStreamHandlerFactory() {
+		super();
+	}
 
-  public static UrlStreamHandlerFactory getInstance() {
-    return instance;
-  }
+	public static UrlStreamHandlerFactory getInstance() {
+		return instance;
+	}
 
-  public static void registerHandlerClass(Class handlerClass) throws Exception {
-    synchronized (UrlStreamHandlerFactory.class) {
-      if (null == instance) {
-        instance = new UrlStreamHandlerFactory();
-        try {
-          URL.setURLStreamHandlerFactory(instance);
-        }
-        catch (Error e) {
-          System.err.println("Another URLStreamHandlerFactory has already been registered");
-          if (BaseUrlStreamHandler.class.isAssignableFrom(handlerClass)){
-        	  System.err.println("This handler is a BaseUrlStreamHandler, so we'll try to register system properties");
-            UrlStreamHandlerDelegate.registerSystemProperty();
-          } else
-            throw new Exception("A URL stream handler factory has been set already! "
-                + "You can try to register a BaseUrlStreamHandler class instead.");
-        }
-      }
-    }
-    String protocol = UrlStreamHandlerFactory.getProtocol(handlerClass);
-    instance.handlerClasses.put(protocol, handlerClass);
-  }
+	public static void registerHandlerClass(Class handlerClass) throws Exception {
+		synchronized (UrlStreamHandlerFactory.class) {
+			if (null == instance) {
+				instance = new UrlStreamHandlerFactory();
+				try {
+					URL.setURLStreamHandlerFactory(instance);
+				}
+				catch (Error e) {
+					logger.finer("Another URLStreamHandlerFactory has already been registered");
+					if (BaseUrlStreamHandler.class.isAssignableFrom(handlerClass)){
+						logger.finer("This handler is a BaseUrlStreamHandler, " +
+							"so we'll try to register system properties");
+						UrlStreamHandlerDelegate.registerSystemProperty();
+					} else
+						throw new Exception("A URL stream handler factory has been set already! "
+							+ "You can try to register a BaseUrlStreamHandler class instead.");
+				}
+			}
+		}
+		String protocol = UrlStreamHandlerFactory.getProtocol(handlerClass);
+		instance.handlerClasses.put(protocol, handlerClass);
+	}
 
-  public static String getProtocol(Class handlerClass) {
-    try {
-      Field[] fields = handlerClass.getFields();
-      for (int i = 0; i < fields.length; i++) {
-        Field f = fields[i];
-        if ("PROTOCOL_NAME".equals(f.getName()))
-          return f.get(null).toString();
-        if (f.getName().startsWith("URL_PROTOCOL_"))
-          return f.get(null).toString();
-      }
-    }
-    catch (Exception ex) {
-      // just a try...
-    }
-    // take the package name for the protocol, if not specified by constant.
-    String pn = handlerClass.getPackage().getName();
-    return pn.substring(pn.lastIndexOf('.') + 1);
-  }
+	public static String getProtocol(Class handlerClass) {
+		try {
+			Field[] fields = handlerClass.getFields();
+			for (int i = 0; i < fields.length; i++) {
+				Field f = fields[i];
+				if ("PROTOCOL_NAME".equals(f.getName()))
+					return f.get(null).toString();
+				if (f.getName().startsWith("URL_PROTOCOL_"))
+					return f.get(null).toString();
+			}
+		}
+		catch (Exception ex) {
+			// just a try...
+		}
+		// take the package name for the protocol, if not specified by constant.
+		String pn = handlerClass.getPackage().getName();
+		return pn.substring(pn.lastIndexOf('.') + 1);
+	}
 
-  public URLStreamHandler createURLStreamHandler(String protocol) {
-    Class handlerClass = (Class)this.handlerClasses.get(protocol);
-    if (null == handlerClass)
-      return null;
-    try {
-      return (URLStreamHandler)handlerClass.newInstance();
-    }
-    catch (Exception ex) {
-      throw new RuntimeException(ex);
-    }
-  }
+	public URLStreamHandler createURLStreamHandler(String protocol) {
+		Class handlerClass = (Class)this.handlerClasses.get(protocol);
+		if (null == handlerClass)
+			return null;
+		try {
+			return (URLStreamHandler)handlerClass.newInstance();
+		}
+		catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
 }
 
