2aa5adb6204463b6cf86a810c9d081a9fe63cb3e
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2009-05-05 16:57:50 +0000

    Add types to navigation events: open and close. Add support to card container to log open and close events.
    git-svn-id: http://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@18631 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/navigation/OTNavigationEvent.java
+++ src/java/org/concord/otrunk/navigation/OTNavigationEvent.java
@@ -4,10 +4,15 @@ import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.OTObjectInterface;
 
 public interface OTNavigationEvent extends OTObjectInterface {
+	public static String OPENED_TYPE = "opened";
+	public static String CLOSED_TYPE = "closed";
 
 	public long getTimestamp();
 	public void setTimestamp(long ms);
 	
 	public OTObject getObject();
 	public void setObject(OTObject object);
+	
+	public String getType();
+	public void setType(String type);
 }
