a68fa2ed4d022a8cf8840a2d584e29fd0765fcc6
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2009-07-16 18:56:21 +0000

    Fix for the first 8-bytes problem, which manifested itself while running under webstart.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@19559 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/net/InstallationCacheRequest.java
+++ src/java/org/concord/otrunk/net/InstallationCacheRequest.java
@@ -1,5 +1,6 @@
 package org.concord.otrunk.net;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -7,14 +8,19 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.CacheRequest;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class InstallationCacheRequest extends CacheRequest
 {
+	private static final Logger logger =
+        Logger.getLogger(InstallationCacheRequest.class.getCanonicalName());
 	private final File localFile;
 	private FileOutputStream fos;
 	private File headersFile;
@@ -76,7 +82,71 @@ public class InstallationCacheRequest extends CacheRequest
 		 */
 		// FIXME This is disabled for now because Netlogo never calls close, and so the cached file ends up zero-length.
 		// FIXME Is there a code snippet which illustrates the problem described above?
-//		ByteArrayOutputStream output = new ByteArrayOutputStream(){
+		ByteArrayOutputStream output = new ByteArrayOutputStream(){
+			private ArrayList<Byte> firstEight = new ArrayList<Byte>(8);
+			private ArrayList<Byte> secondEight = new ArrayList<Byte>(8);
+			private ArrayList<Byte> overflow = new ArrayList<Byte>();
+			private boolean doneWithFirstBytes = false;
+			
+			@Override
+			public void write(byte[] bytes) {
+				try {
+    				int size = bytes.length;
+    				if (! doneWithFirstBytes) {
+    					for (int i = 0; i < size; i++) {
+    						if (firstEight.size() < 8) {
+    							firstEight.add(bytes[i]);
+    						} else if (secondEight.size() < 8) {
+    							secondEight.add(bytes[i]);
+    						} else {
+    							overflow.add(bytes[i]);
+    						}
+    					}
+    					if (overflow.size() > 0) {
+    						doneWithFirstBytes = true;
+    						fos.write(getFirstBytes());
+    						fos.write(convertToByteArray(overflow));
+    					}
+    				} else {
+    					fos.write(bytes);
+    				}
+				} catch (IOException e) {
+					logger.log(Level.SEVERE, "Couldn't write bytes to cache file", e);
+				}
+			}
+			
+			@Override
+			public void write(byte[] bytes, int off, int len) {
+				// super.write(bytes, off, len);
+				byte[] outbytes = new byte[len];
+				System.arraycopy(bytes, off, outbytes, 0, len);
+				write(outbytes);
+			}
+			
+			@Override
+			public void write(int bit) {
+				super.write(bit);
+			}
+			
+			private byte[] getFirstBytes() {
+				// compare the two arrays
+				if (! firstEight.equals(secondEight)) {
+					logger.info("First 8 bytes are NOT duplicated!");
+					firstEight.addAll(secondEight);
+				} else {
+					logger.info("First 8 bytes ARE duplicated");
+				}
+				return convertToByteArray(firstEight);
+			}
+			
+			private byte[] convertToByteArray(ArrayList<Byte> list) {
+				byte[] outBytes = new byte[list.size()];
+				for (int i = 0; i < list.size(); i++) {
+					outBytes[i] = list.get(i);
+				}
+				return outBytes;
+			}
+			
 //			@Override
 //			public void close() throws IOException {
 //				super.close();
@@ -93,9 +163,9 @@ public class InstallationCacheRequest extends CacheRequest
 //				byte[] byteArray = toByteArray();
 //				fos.write(byteArray, offset, contentLength);
 //			}
-//		};
-//		return output;
-		return fos;
+		};
+		return output;
+//		return fos;
 	}
 
 	void writeHeaders(File headerFile, HttpURLConnection conn) throws IOException
