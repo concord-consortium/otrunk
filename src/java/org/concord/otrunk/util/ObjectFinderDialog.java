3410fb924221e2c59db24d0829af97ec3308a4be
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-03-07 20:33:38 +0000

    Better organization for ObjectFinderDialog. Make overlay wrapper popups resizable.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@24346 6e01202a-0783-4428-890a-84243c50cc2b

--- src/java/org/concord/otrunk/util/ObjectFinderDialog.java
+++ src/java/org/concord/otrunk/util/ObjectFinderDialog.java
@@ -1,9 +1,15 @@
 package org.concord.otrunk.util;
 
 import java.awt.Component;
+import java.awt.Dialog;
 import java.awt.Dimension;
+import java.awt.Window;
+import java.awt.event.HierarchyEvent;
+import java.awt.event.HierarchyListener;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -11,6 +17,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -21,20 +28,38 @@ import org.concord.framework.otrunk.OTID;
 import org.concord.framework.otrunk.OTObject;
 import org.concord.framework.otrunk.view.OTObjectTree;
 import org.concord.otrunk.OTrunkImpl;
+import org.concord.otrunk.datamodel.OTDataPropertyReference;
 
 public class ObjectFinderDialog {
+	private static final Logger logger = Logger.getLogger(ObjectFinderDialog.class.getName());
     private OTObject chosenObject;
     private final OTrunkImpl otrunk;
     private final OTObject currentObject;
+    private final OTObject root;
     
-    public ObjectFinderDialog(OTrunkImpl otrunk, OTObject currentObject) {
+    public ObjectFinderDialog(OTrunkImpl otrunk, OTObject currentObject, OTObject root) {
         this.otrunk = otrunk;
         this.currentObject = currentObject;
+        this.root = root;
     }
     
     public OTObject showFinder(Component parent) {
-        JPanel objectPanel = getObjectPanel();
+        final JPanel objectPanel = getObjectPanel();
         objectPanel.setMaximumSize(new Dimension(800,500));
+        
+        // make the option pane resizable
+        objectPanel.addHierarchyListener(new HierarchyListener() {
+            public void hierarchyChanged(HierarchyEvent e) {
+                Window window = SwingUtilities.getWindowAncestor(objectPanel);
+                if (window instanceof Dialog) {
+                    Dialog dialog = (Dialog)window;
+                    if (!dialog.isResizable()) {
+                        dialog.setResizable(true);
+                    }
+                }
+            }
+        });
+        
         int choice = JOptionPane.showConfirmDialog(parent, objectPanel, "Select an object", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
         if (choice == JOptionPane.OK_OPTION) {
             return chosenObject;
@@ -47,22 +72,9 @@ public class ObjectFinderDialog {
         panel.setBorder(BorderFactory.createTitledBorder("Objects"));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         
-        final DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Objects");
-
-        ArrayList<OTObject> multiUserObjects = otrunk.getAllObjects(OTObject.class);
-        
         HashMap<OTID, DefaultMutableTreeNode> childMap = new HashMap<OTID, DefaultMutableTreeNode>();
-        
-        if (currentObject != null && ! multiUserObjects.contains(currentObject)) {
-            DefaultMutableTreeNode mNode = new DefaultMutableTreeNode(currentObject);
-            childMap.put(currentObject.getGlobalId(), mNode);
-            rootNode.add(mNode);
-        }
-        for (OTObject mUser : multiUserObjects) {
-            DefaultMutableTreeNode mNode = new DefaultMutableTreeNode(mUser);
-            childMap.put(mUser.getGlobalId(), mNode);
-            rootNode.add(mNode);
-        }
+
+        final DefaultMutableTreeNode rootNode = populateTree(otrunk, root, childMap);
         
         final JTree tree = new OTObjectTree(rootNode);
         tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
@@ -90,4 +102,66 @@ public class ObjectFinderDialog {
         return panel;
     }
 
+	public static DefaultMutableTreeNode populateTree(OTrunkImpl otrunk, OTObject sourceObject, HashMap<OTID, DefaultMutableTreeNode> knownNodes) {
+		if (knownNodes == null) {
+			knownNodes = new HashMap<OTID, DefaultMutableTreeNode>();
+		}
+		
+		DefaultMutableTreeNode objNode = new DefaultMutableTreeNode(sourceObject);
+		knownNodes.put(sourceObject.getGlobalId(), objNode);
+		
+		ArrayList<OTID> excludeIds = new ArrayList<OTID>();
+		
+		ArrayList<ArrayList<OTDataPropertyReference>> children = otrunk.getOutgoingReferences(sourceObject.getGlobalId(), OTObject.class, true, excludeIds);
+		for (ArrayList<OTDataPropertyReference> childPath : children) {
+//			logger.finer("Next child");
+			for (int i = childPath.size()-1; i >= 0; i--) {
+				OTDataPropertyReference ref = childPath.get(i);
+				DefaultMutableTreeNode parentNode = knownNodes.get(ref.getSource());
+				DefaultMutableTreeNode childNode = knownNodes.get(ref.getDest());
+//				logger.finer(ref.getSource() + " ---> " + ref.getDest());
+				
+				if (parentNode == null) {
+					try {
+//						logger.finer("Creating parent node for: " + ref.getSource());
+						parentNode = new DefaultMutableTreeNode(otrunk.getOTObject(ref.getSource()));
+						knownNodes.put(ref.getSource(), parentNode);
+					} catch (Exception e) {
+						logger.log(Level.WARNING, "Couldn't get OTObject for parent OTID", e);
+					}
+				}
+				
+				if (childNode == null) {
+					try {
+//						logger.finer("Creating child node for: " + ref.getDest());
+						childNode = new DefaultMutableTreeNode(otrunk.getOTObject(ref.getDest()));
+						knownNodes.put(ref.getDest(), childNode);
+					} catch (Exception e) {
+						logger.log(Level.WARNING, "Couldn't get OTObject for child OTID", e);
+					}
+				}
+				
+				if (childNode == null || parentNode == null) {
+//					logger.finer("Creating child or parent node failed.");
+					continue;
+				}
+				
+//				logger.finer("Childnode's path length: " + childNode.getPath().length);
+				
+				if (childNode.getPath().length < 2) {
+//					logger.finer("Adding child: " + childNode + " to parent: " + parentNode);
+					try {
+						parentNode.add(childNode);
+					} catch (IllegalArgumentException e) {
+						// sometimes a node is already somewhere higher up in the tree
+						// in that case, skip it.
+					}
+				} else {
+//					logger.finer("Child: " + childNode + ", parent path: " + childNode.getPath());
+				}
+			}
+		}
+		
+		return objNode;
+	}
 }
