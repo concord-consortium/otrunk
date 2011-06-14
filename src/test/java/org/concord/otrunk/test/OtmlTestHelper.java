a9edc6c0a82c8c8bc7b9c62fa4c9a8ecdebb77db
scytacki <scytacki@6e01202a-0783-4428-890a-84243c50cc2b>
2011-06-14 14:08:25 +0000

    Helper otml based tests
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk@24895 6e01202a-0783-4428-890a-84243c50cc2b

---
+++ src/test/java/org/concord/otrunk/test/OtmlTestHelper.java
@@ -0,0 +1,58 @@
+package org.concord.otrunk.test;
+
+import java.net.URL;
+
+import org.concord.framework.otrunk.OTID;
+import org.concord.framework.otrunk.OTObject;
+import org.concord.framework.otrunk.OTUser;
+import org.concord.framework.otrunk.view.OTView;
+import org.concord.otrunk.OTrunkImpl;
+import org.concord.otrunk.datamodel.OTDatabase;
+import org.concord.otrunk.datamodel.OTTransientMapID;
+import org.concord.otrunk.view.OTConfig;
+import org.concord.otrunk.view.OTViewContainerPanel;
+import org.concord.otrunk.view.OTViewerHelper;
+import org.concord.otrunk.xml.XMLDatabase;
+
+public class OtmlTestHelper
+{
+	private OTViewerHelper viewerHelper;
+	private OTDatabase mainDb;
+	private OTrunkImpl otrunk;
+
+	public void initOtrunk(URL authoredContent) throws Exception {
+		System.setProperty(OTConfig.NO_USER_PROP, "true");
+	    viewerHelper = new OTViewerHelper();
+		mainDb = viewerHelper.loadOTDatabase(authoredContent);
+		viewerHelper.loadOTrunk(mainDb, null);
+		otrunk = (OTrunkImpl) viewerHelper.getOtrunk();
+	}
+
+	public OTObject getObject(String localId) throws Exception {
+		OTID objectId = ((XMLDatabase)viewerHelper.getOtDB()).getOTIDFromLocalID(localId);
+		return otrunk.getRootObjectService().getOTObject(objectId);
+	}	
+	
+	public OTObject getObject(String localId, boolean userVersion) throws Exception {
+		OTObject otObject = getObject(localId);
+		if (userVersion) {
+    		OTUser user = otrunk.getUsers().get(0);
+    		otObject = otrunk.getUserRuntimeObject(otObject, user);
+    		if(!(otObject.getGlobalId() instanceof OTTransientMapID)){
+    			throw new RuntimeException("user object not valid");
+    		}
+		}
+
+		return otObject;
+	}
+	
+	/*
+	 * FIXME the sleep 2000 is pretty annoying here, and it might not always work
+	 */
+	public OTView getView(OTObject object) throws Exception {
+		OTViewContainerPanel panel = viewerHelper.createViewContainerPanel();
+		panel.setCurrentObject(object);
+		Thread.sleep(2000);
+		return panel.getView();
+	}	
+}
